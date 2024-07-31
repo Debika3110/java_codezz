e77060c467ed1afa4c293a205d9f7f605fbbc248
DebikaGhosal <debikaghosal8@gmail.com>
2024-07-31 21:52:01 +0530

    Merge remote-tracking branch 'origin/main'

--- leetcodeprblm.java
+++ leetcodeprblm.java
@@ -7,12 +7,26 @@ public class leetcodeprblm {
     }
 
     public static int helper(int n,int step) {
-        if(n==0){
+        // using recursion
+       /* if(n==0){
             return step;
         }
         if(n%2==0){
             return helper(n/2,step+1);
         }
-        return helper(n-1,step+1);
+        return helper(n-1,step+1);*/
+
+        // using loop
+        while (n>0){
+            if(n%2==0){
+                n=n/2;
+                //step=step+1;
+            }
+            else {
+                n=n-1;
+            }
+            step+=1;
+        }
+        return step;
     }
 }
